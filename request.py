ce25059d60610dfcf60be5c10ad29d60e9b44a62
qiuchunxiao <qiucx@create80.com>
2018-08-23 15:46:39 +0800

    '添加爬虫文件'

---
+++ request.py
@@ -0,0 +1,25 @@
+
+
+import requests
+import io
+import sys
+from bs4 import BeautifulSoup
+
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
+res = requests.get(
+    "https://www.elastic.co/guide/en/logstash/current/index.html")
+# print(res.status_code)
+# print(res.apparent_encoding)
+# print(res.encoding)
+res.encoding = 'UTF-8'
+# print(res.headers)
+# print(res.text)
+
+
+content = res.text
+soup = BeautifulSoup(content, 'html5lib')
+div = soup.find_all(class_="book")
+for i in div:
+    print(i)
+
---
+++ runh.py
@@ -0,0 +1,104 @@
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-18 12
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-18 13
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-18 14
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-18 15
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-18 16
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-18 17
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-18 18
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-18 19
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-18 20
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-18 21
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-18 22
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-18 23
+
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-19 00
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-19 01
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-19 02
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-19 03
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-19 04
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-19 05
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-19 06
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-19 07
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-19 08
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-19 09
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-19 10
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-19 11
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-19 12
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-19 13
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-19 14
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-19 15
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-19 16
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-19 17
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-19 18
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-19 19
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-19 20
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-19 21
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-19 22
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-19 23
+
+
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-20 01
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-20 02
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-20 03
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-20 04
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-20 05
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-20 06
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-20 07
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-20 08
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-20 09
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-20 10
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-20 11
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-20 12
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-20 13
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-20 14
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-20 15
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-20 16
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-20 17
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-20 18
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-20 19
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-20 20
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-20 21
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-20 22
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-20 23
+
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-21 00
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-21 01
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-21 02
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-21 03
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-21 04
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-21 05
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-21 06
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-21 07
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-21 08
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-21 09
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-21 10
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-21 11
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-21 12
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-21 13
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-21 14
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-21 15
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-21 16
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-21 17
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-21 18
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-21 19
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-21 20
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-21 21
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-21 22
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-21 23
+
+
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-22 00
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-22 01
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-22 02
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-22 03
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-22 04
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-22 05
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-22 06
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-22 07
+python /kt_etl/etl_script/owl_cendb_script_v2/deviceReport_H_in.py 2018-01-22 08
+
+
+
+
+
+
